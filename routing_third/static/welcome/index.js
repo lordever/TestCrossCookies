357fe0a347d3ea041c35a1f4166fcc67c3fb6d22
alzh0817 <aleksandr.zheleznov@netcracker.com>
2018-07-26 12:06:29 +0400

    Deleted call of window popup

--- routing_third/static/welcome/index.js
+++ routing_third/static/welcome/index.js
@@ -1,9 +1,6 @@
 window.onload = function () {
-    document.getElementById("windowPopup").addEventListener('click', function () {
-        var win = window.open('http://seconddomain.gre:3005/setcookie', 'test_cookie', 'width=100,height=100,top=10000,left=10000');
-    });
     window.addEventListener('message', function (event) {
         var state = event.data.state;
         document.cookie = "stateFromSecond=" + state;
     });
-}
+};
